5c712886f345832a3fc0ae1f2ef200b1f83a9698
Wes Widner <kai5263499@gmail.com>
2024-01-15 16:59:57 -0500

    change app icon

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "pronthist");
+    gtk_header_bar_set_title(header_bar, "patois");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "pronthist");
+    gtk_window_set_title(window, "patois");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
